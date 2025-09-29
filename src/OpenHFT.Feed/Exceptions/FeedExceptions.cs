5f1863ee487d6483eda19fbd9efbf69857c06d71
Bishopest <jjisoo89@gmail.com>
2025-09-29 12:40:36 +0900

    discrete Exception class about Feed

---
+++ src/OpenHFT.Feed/Exceptions/FeedExceptions.cs
@@ -0,0 +1,53 @@
+using System;
+using OpenHFT.Core.Models;
+
+namespace OpenHFT.Feed.Exceptions;
+
+public class FeedException : Exception
+{
+    public ExchangeEnum SourceExchange { get; }
+
+    public FeedException(ExchangeEnum exchange, string message) : base(message)
+    {
+        SourceExchange = exchange;
+    }
+
+    public FeedException(ExchangeEnum exchange, string message, Exception innerException) : base(message, innerException)
+    {
+        SourceExchange = exchange;
+    }
+}
+
+public class FeedConnectionException : FeedException
+{
+    public Uri ConnectUri { get; }
+
+    public FeedConnectionException(ExchangeEnum exchange, Uri connectUri, string message)
+        : base(exchange, message)
+    {
+        ConnectUri = connectUri;
+    }
+
+    public FeedConnectionException(ExchangeEnum exchange, Uri connectUri, string message, Exception innerException)
+        : base(exchange, message, innerException)
+    {
+        ConnectUri = connectUri;
+    }
+}
+
+public class FeedReceiveException : FeedException
+{
+    public FeedReceiveException(ExchangeEnum exchange, string message, Exception innerException)
+        : base(exchange, message, innerException) { }
+}
+
+public class FeedParseException : FeedException
+{
+    public string RawMessage { get; }
+
+    public FeedParseException(ExchangeEnum exchange, string rawMessage, string message, Exception innerException)
+        : base(exchange, message, innerException)
+    {
+        RawMessage = rawMessage;
+    }
+}
